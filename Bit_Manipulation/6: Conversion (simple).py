5e94cb9237c75c4ca675a89a3da2a5baad3f92d1
Naila <nfatima5596@gmail.com>
2023-03-28 15:15:24 -0500

    Create 6: Conversion (simple).py

---
+++ Bit_Manipulation/6: Conversion (simple).py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        
+        xor = start ^ goal
+        
+        count = 0
+        while xor > 0:
+            if xor & 1 == 1: # set bit
+                count += 1
+            xor = xor >> 1
+        return count
